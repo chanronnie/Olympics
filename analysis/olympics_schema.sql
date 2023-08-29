e6044b3372e74be0870172ff993003b382655aab
Ronnie Chan <ronniekkc@gmail.com>
2023-08-29 18:13:30 -0400

    Create files for analysis

---
+++ analysis/olympics_schema.sql
@@ -0,0 +1,41 @@
+/*
+##########################################################
+This SQL script imports datasets and creates tables.
+##########################################################
+*/
+
+
+-- Create Schema
+DROP SCHEMA IF EXISTS olympics CASCADE;
+CREATE SCHEMA olympics;
+
+
+-- Create Table "olympic_history"
+DROP TABLE IF EXISTS olympics.olympic_history;
+CREATE TABLE olympics.olympic_history
+(
+	id		INTEGER,
+	name 	VARCHAR,
+	gender 	VARCHAR,
+	born 	VARCHAR,
+	died	VARCHAR,
+	height 	VARCHAR,
+	weight 	VARCHAR, 	
+	team 	VARCHAR,
+	game 	VARCHAR,
+	noc 	VARCHAR,
+	sport 	VARCHAR,
+	event 	VARCHAR,
+	medal 	VARCHAR
+);
+
+
+-- Create Table "host_cities"
+DROP TABLE IF EXISTS olympics.host_cities;
+CREATE TABLE olympics.host_cities
+(
+	year	INTEGER,
+	season 	VARCHAR,
+	game 	VARCHAR,
+	host_city 	VARCHAR
+);
